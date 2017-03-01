d428cf40219ec7b897d3050946d641a961b82711
Marcos <mvro.dev@gmail.com>
2017-03-01 09:31:11 -0800

    Shell compiler for make .jar with dependencies, required (sbt)

---
+++ compiler.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if ! type sbt &> /dev/null; then
+	echo "Install SBT"
+	exit 1
+fi
+
+sparkVer=$(spark-submit --version 2>&1 | awk '{if(match($0, /version ([0-9\.]+$)/, v)){print v[1]}}')
+
+cat <<EOF > script.scala
+System.exit(0)
+EOF
+
+scalaVer=$(spark-shell -i script.scala 2>&1 | awk '{if(match($0,/Scala version ([0-9\.]+)/,s)){print s[1]}}')
+
+bigShipperVer="0.1"
+
+rm -f script.scala
+
+echo "Creating build.sbt with Spark version ${sparkVer} and Scala version ${scalaVer}"
+
+cat <<EOF > build.sbt
+name := "BigShipper"
+
+version := "$bigShipperVer"
+
+scalaVersion := "$scalaVer"
+
+libraryDependencies += "com.github.scopt" %% "scopt" % "3.5.0"
+libraryDependencies += "org.tsers.zeison" %% "zeison" % "0.7.0"
+libraryDependencies += "log4j" % "log4j" % "1.2.14"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "$sparkVer" % "provided"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "$sparkVer" % "provided"
+libraryDependencies += "org.apache.spark" %% "spark-hive" % "$sparkVer" % "provided"
+EOF
+
+echo "JAR making..."
+
+sbt assembly
+
+echo -e "\nJAR compiled.\nRun example: spark-submit --class main.Shipper target/scala-${scalaVer%.*}/BigShipper-assembly-${bigShipperVer}.jar -c /path/config.json --loglevel debug"
