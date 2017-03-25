362821b173e75f2e96b5791f3aea4d50bf1ab1e5
mvrpl <mvro.dev@gmail.com>
2017-03-25 20:26:11 +0000

    Change split delimited files to databricks:spark-csv

--- src/main/scala/shipper.scala
+++ src/main/scala/shipper.scala
@@ -1,49 +1,28 @@
 package shipper
 
 import org.tsers.zeison.Zeison
-import utils.{Utils, Spark}
+import utils.{Utils, Spark, Logs}
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.types._
 
-class Loader {
+class Loader extends Logs {
 
 	val utils = new Utils
 	val spark = new Spark
 
 	def delimitedFiles(configs: Zeison.JValue) : Boolean = {
 		val fields = configs.SOURCE.FIELDS.map(f => "%s:%s".format(f.NAME.toStr, f.TYPE.toStr.toLowerCase)).mkString(",")
-		val schema = utils.makeSchema(fields)
+		val cSchema = utils.makeSchema(fields)
 		val dirRawFiles = configs.SOURCE.DIR_RAW_FILES.toStr
 		val delimiter = configs.SOURCE.DELIMITER_RAW.toStr
-		val rawdata = spark.sc.textFile(dirRawFiles).mapPartitions(_.drop(1))
-		val rowsRDD = rawdata.map(p => {
-			var list: collection.mutable.Seq[Any] = collection.mutable.Seq.empty[Any]
-			var index = 0
-			if (configs.SOURCE.QUOTEFIELD.isDefined) {
-				val quote = configs.SOURCE.QUOTEFIELD.toStr
-				val fields = p.split("\\%s(?=([^\\%s]*\\%s[^\\%s]*\\%s)*[^\\%s]*$)".format(delimiter,quote,quote,quote,quote,quote)).map(_.stripPrefix("\\%s".format(quote)).stripSuffix("\\%s".format(quote)))
-			} else {
-				var fields = p.split(delimiter.charAt(0))
-			}
-			fields.foreach(value => {
-				var valType = schema.fields(index).dataType
-				var returnVal: Any = null
-				valType match {
-					case IntegerType => returnVal = value.toString.toInt
-					case ShortType => returnVal = value.toString.toInt
-					case DoubleType => returnVal = value.toString.toDouble
-					case LongType => returnVal = value.toString.toLong
-					case FloatType => returnVal = value.toString.toFloat
-					case ByteType => returnVal = value.toString.toByte
-					case StringType => returnVal = value.toString
-					case TimestampType => returnVal = value.toString
-				}
-				list = list :+ returnVal
-				index += 1
-			})
-			Row.fromSeq(list)
-		})
-		val dataFrame = spark.makeDF(rowsRDD, schema)
+		var headerFile = ""
+		if (configs.SOURCE.HEADER.isBool) {
+			headerFile = configs.SOURCE.HEADER.toBool.toString
+		} else {
+			error("The option SOURCE.HEADER need a boolean type.")
+			System.exit(1)
+		}
+		val dataFrame = spark.hiveContext.read.format("com.databricks.spark.csv").option("header", headerFile).option("delimiter", delimiter).schema(cSchema).load(dirRawFiles)
 		if (configs.TARGET.ACTION.toStr.toLowerCase == "update"){
 			val targetTable = configs.TARGET.HIVE_TABLE.toStr
 			val targetDF = spark.hiveContext.sql(s"select * from $targetTable").toDF
