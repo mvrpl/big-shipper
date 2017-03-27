c38bc8c0826104273501afcfb2485376df0a9a49
mvrpl <mvro.dev@gmail.com>
2017-03-27 00:14:44 +0000

    Firsts automated test

--- src/test/scala/BigShipperTests.scala
+++ src/test/scala/BigShipperTests.scala
@@ -4,7 +4,12 @@ import org.scalatest.FunSuite
 import com.holdenkarau.spark.testing.SharedSparkContext
 
 class BigShipperTests extends FunSuite with SharedSparkContext{
-	test("First test") {
-		assert(1 == 1)
+	import org.apache.spark.sql.types._
+	System.setProperty("loglevel", "ERROR")
+	test("Checked utils.makeSchema") {
+		val loader = new Loader(sc)
+		val expected = new StructType(Array(StructField("field1",IntegerType,true), StructField("field2",StringType,true), StructField("field3",BooleanType,true), StructField("field4",TimestampType,true), StructField("field5",FloatType,true)))
+		val result = loader.utils.makeSchema("field1:int,field2:string,field3:boolean,field4:date,field5:float")
+		assert(result == expected)
 	}
 }
