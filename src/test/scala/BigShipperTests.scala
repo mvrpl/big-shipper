6a5a04a6d02759f81a70c362b3c1c5146440ebaa
mvrpl <mvro.dev@gmail.com>
2017-04-22 22:54:35 +0000

    Add test

--- src/test/scala/BigShipperTests.scala
+++ src/test/scala/BigShipperTests.scala
@@ -1,15 +1,23 @@
 package shipper
 
+import java.io._
+import org.tsers.zeison.Zeison
 import org.scalatest.FunSuite
 import com.holdenkarau.spark.testing.SharedSparkContext
 
 class BigShipperTests extends FunSuite with SharedSparkContext{
 	import org.apache.spark.sql.types._
-	System.setProperty("loglevel", "ERROR")
+	System.setProperty("loglevel", "FATAL")
 	test("Checked utils.makeSchema") {
 		val loader = new Loader(sc)
 		val expected = new StructType(Array(StructField("field1",IntegerType,true), StructField("field2",StringType,true), StructField("field3",BooleanType,true), StructField("field4",TimestampType,true), StructField("field5",FloatType,true)))
 		val result = loader.utils.makeSchema("field1:int,field2:string,field3:boolean,field4:date,field5:float")
+		assert(result == expected)
+	}
+	test("Checked utils.fieldTypeInSchema") {
+		val loader = new Loader(sc)
+		val expected = LongType
+		val result = loader.utils.fieldTypeInSchema("bigint")
 		assert(result == expected)
 	}
 }
